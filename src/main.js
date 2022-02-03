fcd1edf2ba6f03009f3e1997c438ef2c48eae270
ValeriKozarev <val.kozarev.20@gmail.com>
2022-02-03 11:24:50 -0600

    removing hashmark from the url

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ const routes = [
 
 // instantiate router
 const router = VueRouter.createRouter({
-  history: VueRouter.createWebHashHistory(),
+  history: VueRouter.createWebHistory(),
   routes: routes
 })
 
