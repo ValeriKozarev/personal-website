b85ff8f66c454437d576304d067f52b272bf3b81
ValeriKozarev <val.kozarev.20@gmail.com>
2022-02-03 20:29:25 -0600

    adding some more data

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -2,7 +2,7 @@
   <div class="card">
     <div class="card-content">
       <div class="content">
-        this is the page home component!
+        Hi! Welcome to my website. This is the home page and I don't know what to put here.
       </div>
     </div>
   </div>
