a23be44c52801527c2f2fa5481237e1bb7d56e0b
ValeriKozarev <val.kozarev.20@gmail.com>
2022-02-03 21:00:01 -0600

    adding some placeholder data and styling for homepage

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -6,12 +6,28 @@
       </div>
     </div>
   </div>
+  <section class="section">
+    <h1 class="title">Technologies</h1>
+    <h2 class="subtitle">Here is some stuff I can use</h2>
+    <div class="columns is-multiline">
+      <div v-for="technology in technologies" :key="technology.name" class="column is-one-fifth">
+        {{technology.name}}
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+
 export default {
   name: 'PageHome',
   props: {
+  },
+  data () {
+    return {
+      technologies: sourceData.technologies
+    }
   }
 }
 </script>
