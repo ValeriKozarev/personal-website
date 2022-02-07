062aa624213c36ba71c1b5b8e420dfebaac0eafb
ValeriKozarev <val.kozarev.20@gmail.com>
2022-02-07 08:27:05 -0600

    updating the look and feel of the hero

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,13 +1,18 @@
 <template>
-  <section class="hero  is-primary is-halfheight">
+  <section class="hero is-halfheight">
     <div class="hero-body">
       <div class="container has-text-centered">
-        <p class="title">
-          Hey, there! I'm Val. &#128075;&#127996;
-        </p>
-        <p class="subtitle">
-          Welcome to my website. I'm a coffee lover, a tree hugger, and a musician. I'm also a Software Engineer. I am interested in learning new frameworks, developing interesting and beautiful applications, and making technology accessible to as many people as possible. Feel free to look around and get to know me!
-        </p>
+        <div class="hero-text">
+          <p class="title">
+            Hey, there! I'm Val. &#128075;&#127996;
+          </p>
+          <p class="subtitle">
+            Welcome to my website. I'm a coffee lover, a tree hugger, and a musician. I'm also a Software Engineer. I am interested in learning new frameworks, developing interesting and beautiful applications, and making technology accessible to as many people as possible. Feel free to look around and get to know me!
+          </p>
+        </div>
+        <div class="hero-pic">
+          <img src="../../public/hero_pic.jpg" alt="picture of Val">
+        </div>
       </div>
     </div>
   </section>
