457d165082b0a3cbd3ff4d1e7baba98ecf7d0f4f
ValeriKozarev <val.kozarev.20@gmail.com>
2022-02-15 10:11:50 -0600

    undoing mistake with webpack, re-adding randomize button, removing some data

--- src/components/PageAbout.vue
+++ src/components/PageAbout.vue
@@ -1,74 +1,34 @@
 <template>
   <section class="section">
-    <h1 class="title">About Me</h1>
-    <h2 class="subtitle">There's a bit more to everyone than just their job title, don't you think?</h2>
+    <h2 class="title">About Me</h2>
+    <h3 class="subtitle">There's a bit more to everyone than just their job title, don't you think?</h3>
     <div class="container has-text-centered">
       <br />
       <p>
-        I was born in Bulgaria. I studied Computer Science and Statistics at NC State in Raleigh, NC. Currently, I am a software engineer at ______. I have experience with and am interested in Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I am a first-generation immigrant from Bulgaria currently working as a Software Engineer in the United States. I studied Computer Science and Statistics at NC State University in Raleigh, NC. I have experience in a variety of industries and with a variety of technologies. Currently, I am really interested in front-end development and cloud computing.
       </p>
       <br />
       <p>
-        I am passionate about Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I care deeply about the climate crisis, as well as the current health and wealth inequalities in the United States.
       </p>
       <br />
       <p>
-        I like camping, travelling and Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        I really enjoy drinking coffee, playing guitar, and spending time in the great outdoors.
       </p>
     </div>
     <br />
   </section>
 
   <section class="section">
-    <h1 class="title">Some of my Favorite Albums Are</h1>
+    <h2 class="title">Some of my Favorite Albums Are</h2>
+    <button class="randomize" @click="getRandomAlbums">Randomize</button>
     <div class="columns is-multiline">
       <div v-for="album in albums" :key="album.title" class="column is-one-quarter">
         <div class="card album">
           <div class="card-image">
             <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              <img :src="'../assets/' + album.artwork" :alt="'' + album.title + ' by ' + album.artist">
             </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-5">{{ album.title }}</p>
-            <p class="subtitle is-6">{{ album.artist }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="section">
-    <h1 class="title">Some of my Favorite Books Are</h1>
-    <div class="columns is-multiline">
-      <div v-for="book in books" :key="book.title" class="column is-one-quarter">
-        <div class="card book">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-5">{{ book.title }}</p>
-            <p class="subtitle is-6">{{ book.author }}</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section class="section">
-    <h1 class="title">Some of my Favorite Stories Are</h1>
-    <div class="columns is-multiline">
-      <div v-for="game in games" :key="game.title" class="column is-one-quarter">
-        <div class="card game">
-          <div class="card-image">
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-5">{{ game.title }}</p>
           </div>
         </div>
       </div>
@@ -85,17 +45,18 @@ export default {
   },
   data () {
     return {
-      albums: sourceData.albums,
-      games: sourceData.games,
-      books: sourceData.books
+      albums: this.getInitAlbums()
     }
   },
   methods: {
-    getRandomizedAlbums () {
-      // TODO: implement a randomized selection from array of album objects
-      return this.albums[0]
+    getInitAlbums () {
+      const shuffled = sourceData.albums.sort(() => 0.5 - Math.random())
+      return shuffled.slice(0, 4)
+    },
+    getRandomAlbums () {
+      const shuffled = sourceData.albums.sort(() => 0.5 - Math.random())
+      this.albums = shuffled.slice(0, 4)
     }
-    // TODO: implemented randomized selection methods for other arrays
   }
 }
 </script>
