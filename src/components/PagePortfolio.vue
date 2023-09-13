9aa0075ca665754a24ecb800c8fe66ebbdf6611f
ValeriKozarev <val.kozarev.20@gmail.com>
2023-09-13 13:30:52 -0600

    small tweaks to styling on portfolio page

--- src/components/PagePortfolio.vue
+++ src/components/PagePortfolio.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="section">
-    <h1 class="title">Relevant Experience</h1>
-    <h2 class="subtitle">
-      I am a Software Engineer with experience in full-stack development and building SPAs. I am currently interested in web development, accessibility, and cloud computing.
-    </h2>
+    <div class="section-header">
+      <h1 class="title">Relevant Experience</h1>
+      <h2 class="subtitle">
+        I am a Software Engineer with experience in full-stack development and building SPAs. I am currently interested in web development, accessibility, and cloud computing.
+      </h2>
+    </div>
     <br />
     <div class="columns is-multiline is-mobile">
       <div v-for="job in jobs" :key="job.title" class="column is-10 is-offset-1 portfolio-entry">
@@ -28,10 +30,12 @@
   </section>
 
   <section class="section">
-    <h1 class="title">Projects</h1>
-    <h2 class="subtitle">
-      I use personal projects as a way to learn new skills. Check out my GitHub for more things that I've made!
-    </h2>
+    <div class="section-header">
+      <h1 class="title">Projects</h1>
+      <h2 class="subtitle">
+        I use personal projects as a way to learn new skills. Check out my GitHub for more things that I've made!
+      </h2>
+    </div>
     <br />
     <div class="columns is-multiline is-mobile">
       <div v-for="project in projects" :key="project.title" class="column is-10 is-offset-1 portfolio-entry">
@@ -82,6 +86,12 @@ export default {
 </script>
 
 <style scoped>
+.section-header {
+  background-color: #fafafa;
+  border-radius: 20px;
+  padding: 2rem;
+}
+
 .portfolio-entry {
   padding: 2em 0em;
 }
